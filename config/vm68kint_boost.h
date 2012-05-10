385924a5de470dfd648b874fe936dc338031eae7
Kaz Sasa <kazssym@vx68k.org>
2012-05-10 22:48:23 +0900

    Added compiler-dependent headers for <vm68kint.h>.

---
+++ config/vm68kint_boost.h
@@ -0,0 +1,23 @@
+#ifndef vm68kint_boostH
+#define vm68kint_boostH 1
+
+#include <boost/cstdint.hpp>
+
+namespace vm68k
+{
+	using ::boost::int_least8_t;
+	using ::boost::uint_least8_t;
+	using ::boost::int_least16_t;
+	using ::boost::uint_least16_t;
+	using ::boost::int_least32_t;
+	using ::boost::uint_least32_t;
+
+	using ::boost::int_fast8_t;
+	using ::boost::uint_fast8_t;
+	using ::boost::int_fast16_t;
+	using ::boost::uint_fast16_t;
+	using ::boost::int_fast32_t;
+	using ::boost::uint_fast32_t;
+}
+
+#endif /* once */
---
+++ config/win32/vm68kint.h
@@ -0,0 +1,10 @@
+#ifndef vm68kintH
+#define vm68kintH 1
+
+#ifdef __BORLANDC__
+#include <vm68kint_boost.h>
+#else
+#error Unknown compiler
+#endif
+
+#endif /* once */

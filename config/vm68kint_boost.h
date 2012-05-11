3bd40331ec0f2d98be2117aa59d71cf20646c8d8
Kaz Sasa <kazssym@vx68k.org>
2012-05-11 22:26:10 +0900

    Removed tabs.

--- config/vm68kint_boost.h
+++ config/vm68kint_boost.h
@@ -5,19 +5,19 @@
 
 namespace vm68k
 {
-	using ::boost::int_least8_t;
-	using ::boost::uint_least8_t;
-	using ::boost::int_least16_t;
-	using ::boost::uint_least16_t;
-	using ::boost::int_least32_t;
-	using ::boost::uint_least32_t;
+    using boost::int_least8_t;
+    using boost::uint_least8_t;
+    using boost::int_least16_t;
+    using boost::uint_least16_t;
+    using boost::int_least32_t;
+    using boost::uint_least32_t;
 
-	using ::boost::int_fast8_t;
-	using ::boost::uint_fast8_t;
-	using ::boost::int_fast16_t;
-	using ::boost::uint_fast16_t;
-	using ::boost::int_fast32_t;
-	using ::boost::uint_fast32_t;
+    using boost::int_fast8_t;
+    using boost::uint_fast8_t;
+    using boost::int_fast16_t;
+    using boost::uint_fast16_t;
+    using boost::int_fast32_t;
+    using boost::uint_fast32_t;
 }
 
 #endif /* once */
--- config/vm68kint_classic.h
+++ config/vm68kint_classic.h
@@ -3,19 +3,19 @@
 
 namespace vm68k
 {
-	typedef signed char    int_least8_t;
-	typedef unsigned char  uint_least8_t;
-	typedef short          int_least16_t;
-	typedef unsigned short uint_least16_t;
-	typedef int            int_least32_t;
-	typedef unsigned int   uint_least32_t;
+    typedef signed char    int_least8_t;
+    typedef unsigned char  uint_least8_t;
+    typedef short          int_least16_t;
+    typedef unsigned short uint_least16_t;
+    typedef int            int_least32_t;
+    typedef unsigned int   uint_least32_t;
 
-	typedef int            int_fast8_t;
-	typedef int            uint_fast8_t;
-	typedef int            int_fast16_t;
-	typedef int            uint_fast16_t;
-	typedef int            int_fast32_t;
-	typedef unsigned int   uint_fast32_t;
+    typedef int            int_fast8_t;
+    typedef unsigned int   uint_fast8_t;
+    typedef int            int_fast16_t;
+    typedef unsigned int   uint_fast16_t;
+    typedef int            int_fast32_t;
+    typedef unsigned int   uint_fast32_t;
 }
 
 #endif /* once */

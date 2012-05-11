a6a386f2abb2c8982d2f911e519efe73f707e956
Kaz Sasa <kazssym@vx68k.org>
2012-05-11 22:32:02 +0900

    Added methods.

--- src/vm68kdef.h
+++ src/vm68kdef.h
@@ -21,6 +21,19 @@ namespace vm68k
     public:
         typedef int_least8_t int_type;
         typedef uint_least8_t uint_type;
+
+        /**
+         * <stereotype>constructor</stereotype>
+         */
+        byte();
+
+        /**
+         * <stereotype>constructor</stereotype>
+         */
+        byte(uint_fast8_t x);
+
+        operator int_fast8_t() const ;
+
     private:
         uint_least8_t value;
     };
@@ -30,6 +43,19 @@ namespace vm68k
     public:
         typedef int_least16_t int_type;
         typedef uint_least16_t uint_type;
+
+        /**
+         * <stereotype>constructor</stereotype>
+         */
+        word();
+
+        /**
+         * <stereotype>constructor</stereotype>
+         */
+        word(uint_fast16_t x);
+
+        operator int_fast16_t() const;
+
     private:
         uint_least16_t value;
     };
@@ -39,10 +65,68 @@ namespace vm68k
     public:
         typedef int_least32_t int_type;
         typedef uint_least32_t uint_type;
+
+        /**
+         * <stereotype>constructor</stereotype>
+         */
+        long_word();
+
+        /**
+         * <stereotype>constructor</stereotype>
+         */
+        long_word(uint_fast32_t x);
+
+        operator int_fast32_t() const;
+
     private:
         uint_least32_t value;
     };
+
     typedef long_word lword;
+
+    /* Inline implementations.  */
+
+    inline byte::byte()
+    {
+    }
+
+    inline byte::byte(uint_fast8_t x)
+    {
+        value = x & 0xff;
+    }
+
+    inline byte::operator int_fast8_t() const
+    {
+        return value;
+    }
+
+    inline word::word()
+    {
+    }
+
+    inline word::word(uint_fast16_t x)
+    {
+        value = x & 0xffff;
+    }
+
+    inline word::operator int_fast16_t() const
+    {
+        return value;
+    }
+
+    inline long_word::long_word()
+    {
+    }
+
+    inline long_word::long_word(uint_fast32_t x)
+    {
+        value = x;
+    }
+
+    inline long_word::operator int_fast32_t() const
+    {
+        return value;
+    }
 }
 
 #endif

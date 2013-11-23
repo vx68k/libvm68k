f30bb25b9772ed954461bcc55c2a14aec5e07c7d
Kaz Nishimura <kazssym@vx68k.org>
2013-11-23 20:59:13 +0900

    Updated the copyright holder's name.

--- src/bits/vm68kcontext.h
+++ src/bits/vm68kcontext.h
@@ -1,6 +1,6 @@
 /*
  * vm68kcontext - execution contexts (interface)
- * Copyright (C) 2012  Kaz Sasa
+ * Copyright (C) 2012 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/bits/vm68kmemory.cpp
+++ src/bits/vm68kmemory.cpp
@@ -1,6 +1,6 @@
 /*
  * Memory components.
- * Copyright (C) 2012  Kaz Sasa
+ * Copyright (C) 2012 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

c1fd502b0e41afa6e6203a454553f28ef1d4c370
Kaz Nishimura <kazssym@linuxfront.com>
2020-07-21 15:10:27 +0900

    Change the conditions to define '_VM68KCORE_PUBLIC'

--- libvm68k/bits/vm68kcore.h
+++ libvm68k/bits/vm68kcore.h
@@ -21,18 +21,20 @@
 
 #ifndef _VM68KCORE_PUBLIC
 #if _WIN32
-#if VM68KCORE_DLL
+#if _LIBVM68K
+#if DLL_EXPORT
 #define _VM68KCORE_PUBLIC __declspec(dllexport)
-#else
-#define _VM68KCORE_PUBLIC __declspec(dllimport)
 #endif
-#else /* !_WIN32 */
+#else /* not _LIBVM68K */
+#define _VM68KCORE_PUBLIC __declspec(dllimport)
+#endif /* not _LIBVM68K */
+#else /* not _WIN32 */
 #if defined __has_attribute
 #if __has_attribute(visibility)
 #define _VM68KCORE_PUBLIC __attribute__((visibility("default")))
 #endif
 #endif /* defined __has_attribute */
-#endif /* !_WIN32 */
+#endif /* not _WIN32 */
 #endif
 
 #ifndef _VM68KCORE_PUBLIC

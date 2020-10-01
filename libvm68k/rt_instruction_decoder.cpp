9ce3e3d64b05392bdd114645a96e1520a061fbca
Kaz Nishimura <kazssym@linuxfront.com>
2020-10-01 17:15:57 +0900

    Add new class 'rt_instruction_decoder'

---
+++ libvm68k/rt_instruction_decoder.cpp
@@ -0,0 +1,36 @@
+// rt_instruction_decoder.cpp
+// Copyright (C) 2020 Kaz Nishimura
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <bits/vm68k/rt_instruction_decoder.h>
+
+using namespace vm68k;
+
+rt_instruction_decoder::~rt_instruction_decoder()
+{
+    // Nothing to do.
+}
+
+long_word rt_instruction_decoder::decode(long_word pc, executor &e) const
+{
+    // TODO: implement this function.
+    return pc;
+}

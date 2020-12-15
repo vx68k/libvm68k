afd5f711044dfb577706640da0ea90196e44f08c
Kaz Nishimura <kazssym@linuxfront.com>
2020-12-15 10:17:23 +0900

    Move register class definitions

--- libvm68k/bits/vm68k/execution_context.h
+++ libvm68k/bits/vm68k/execution_context.h
@@ -21,6 +21,7 @@
 
 #include <bits/vm68k/register_file.h>
 #include <vm68k/memory>
+#include <vm68k/data>
 #include <array>
 #include <memory>
 #include <utility>
@@ -28,6 +29,101 @@
 
 namespace vm68k
 {
+    /**
+     * Data registers.
+     */
+    class _VM68K_PUBLIC data_register
+    {
+    private:
+        long_word::uint_type _value;
+
+    public:
+        data_register &operator =(const long_word value)
+        {
+            _value = value.to_uint();
+            return *this;
+        }
+
+        data_register &operator =(const word value)
+        {
+            auto unchanged = _value & 0xffff0000U;
+            _value = unchanged | value.to_uint();
+            return *this;
+        }
+
+        data_register &operator =(const byte value)
+        {
+            auto unchanged = _value & 0xffffff00U;
+            _value = unchanged | value.to_uint();
+            return *this;
+        }
+
+    public:
+        /**
+         * Converts the register value to a long word.
+         */
+        operator long_word() const noexcept
+        {
+            return long_word(_value);
+        }
+
+        /**
+         * Converts the register value to a word.
+         */
+        operator word() const noexcept
+        {
+            return word(_value);
+        }
+
+        /**
+         * Converts the register value to a byte.
+         */
+        operator byte() const noexcept
+        {
+            return byte(_value);
+        }
+    };
+
+    /**
+     * Address registers.
+     */
+    class _VM68K_PUBLIC address_register
+    {
+    private:
+        long_word::uint_type _value;
+
+    public:
+        address_register &operator =(const long_word value)
+        {
+            _value = value.to_int();
+            return *this;
+        }
+
+        address_register &operator =(const word value)
+        {
+            // Word assignments shall be sign-extended.
+            _value = value.to_int();
+            return *this;
+        }
+
+    public:
+        /**
+         * Converts the register value to a long word.
+         */
+        operator long_word() const noexcept
+        {
+            return long_word(_value);
+        }
+
+        /**
+         * Converts the register value to a word.
+         */
+        operator word() const noexcept
+        {
+            return word(_value);
+        }
+    };
+
     /*
      * Execution contexts.
      */

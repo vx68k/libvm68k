e72ea7fd978aa7ce44d74bd8cba613cdd391a3d4
Kaz Nishimura <kazssym@linuxfront.com>
2020-12-22 19:38:23 +0900

    Add 'device_manager.cpp'

---
+++ libvm68kapi/device_manager.cpp
@@ -0,0 +1,51 @@
+// device_manager.cpp
+// Copyright (C) 2020 Kaz Nishimura
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <bits/vm68k/device_manager.h>
+
+#include <utility>
+
+using std::move;
+using std::shared_ptr;
+using namespace vm68k;
+
+device_manager::device_manager()
+{
+    // Nothing to do.
+}
+
+device_manager::device_manager(device_manager &&other) noexcept
+:
+    _devices {move(other._devices)}
+{
+    // Nothing to do.
+}
+
+device_manager::~device_manager()
+{
+    // Nothing to do.
+}
+
+void device_manager::add_device(const shared_ptr<device> &d)
+{
+    // TODO: Implement this function.
+}

19ec8b6234ca2e63b427531d0fb6e496cc0ea36d
Kaz Nishimura <kazssym@linuxfront.com>
2020-12-25 17:42:34 +0900

    Add new test fixture for class device_manager

--- test/test_device.cpp
+++ test/test_device.cpp
@@ -24,3 +24,44 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/TestFixture.h>
+
+using std::shared_ptr;
+using std::make_shared;
+using CppUnit::TestFixture;
+using namespace vm68k;
+
+
+/*
+ * Tests for class device_manager.
+ */
+class DeviceManagerTests: public TestFixture
+{
+    CPPUNIT_TEST_SUITE(DeviceManagerTests);
+    CPPUNIT_TEST(testMemory);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    shared_ptr<memory_map> _memory_map;
+    shared_ptr<device_manager> _device_manager;
+
+public:
+    virtual void setUp() override
+    {
+        _memory_map = make_shared<paged_memory_map>();
+        _device_manager = make_shared<device_manager>(_memory_map);
+    }
+
+public:
+    virtual void tearDown() override
+    {
+        _device_manager.reset();
+        _memory_map.reset();
+    }
+
+private:
+    void testMemory()
+    {
+        CPPUNIT_ASSERT(_memory_map == _device_manager->memory());
+    }
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(DeviceManagerTests);

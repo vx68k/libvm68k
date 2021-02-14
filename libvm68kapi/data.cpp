a95767130e8256e09de2a7ae95d0a89d211a1d69
Kaz Nishimura <kazssym@linuxfront.com>
2021-02-14 22:30:54 +0900

    Update the copyright notice

--- libvm68kapi/data.cpp
+++ libvm68kapi/data.cpp
@@ -1,5 +1,5 @@
 // data.cpp
-// Copyright (C) 2012-2019 Kaz Nishimura
+// Copyright (C) 2012-2021 Kaz Nishimura
 //
 // This program is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the Free

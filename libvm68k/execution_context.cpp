ce18d1c100082367d43c1053fa3c36291d06843c
Kaz Nishimura <kazssym@linuxfront.com>
2021-02-16 07:54:18 +0900

    Update the copyright notice

--- libvm68k/execution_context.cpp
+++ libvm68k/execution_context.cpp
@@ -1,5 +1,5 @@
 // execution_context.cpp
-// Copyright (C) 2012-2020 Kaz Nishimura
+// Copyright (C) 2012-2021 Kaz Nishimura
 //
 // This program is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the Free
